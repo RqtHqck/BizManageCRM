c8d7ba0e46b731cbfdec8a7591b61817110b54bc
Ihar <rathack13@gmail.com>
2024-09-22 23:34:19 +0300

    MongoDB is connected with app. Atlas MongoDB service was used for remote MongoDB database.

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser')
+require('dotenv').config({ path: './config/.env' });
 // Routes -------------------------------------------
 const authRoutes = require('./routes/auth')
 const orderRoutes = require('./routes/order')
 const categoryRoutes = require('./routes/category')
 const positionRoutes = require('./routes/position')
 const analyticsRoutes = require('./routes/analytics')
+// Other requirements
+const keys = require('./config/keys')
+
 
 const app = express();
 
-app.use(require('morgan')('dev'))
-app.use(require('cors')())
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB database connected'))
+  .catch(error => console.error(error))
+
+app.use(require('morgan')('dev')) //
+app.use(require('cors')())  // Cors attacks defend
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
@@ -20,8 +29,6 @@ app.use('/api/order', orderRoutes)
 app.use('/api/category', categoryRoutes)
 app.use('/api/position', positionRoutes)
 app.use('/api/analytics', analyticsRoutes)
-
-
 
 
 module.exports = app
